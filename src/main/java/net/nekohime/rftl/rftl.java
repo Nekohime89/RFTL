49ea1310997dd449f5eb1a6b014e2cc81d6063b6
Matthew Ball <palpaleos89@gmai.com>
2020-12-20 08:31:03 -0700

    Initial Commit

---
+++ src/main/java/net/nekohime/rftl/rftl.java
@@ -0,0 +1,10 @@
+package net.nekohime.rftl;
+
+import net.fabricmc.api.ModInitializer;
+
+public class rftl implements ModInitializer {
+	public static String MOD_ID = "rftl";
+	@Override
+	public void onInitialize() {
+	}
+}
